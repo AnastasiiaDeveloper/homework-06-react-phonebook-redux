81d264031f3f85c8a4e678e976bcf749a864eb5f
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-11-19 21:26:53 +0200

    ready for check

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import MainBlockInp from "./components/input-comp/main-block-inp";
+import List from "./components/list/list";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center">
+        <MainBlockInp />
+        <List />
+      </div>
+    </div>
+  );
+}
+
+export default App;
